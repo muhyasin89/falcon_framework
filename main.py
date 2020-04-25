7e94e59065850eb36d9d911184c7fbc6260e9cf5
muhyasin89 <muhyasin89@gmail.com>
2020-04-26 06:06:53 +0700

    [UPDATE]change main python

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import falcon
+import falcon, json
 
 class MainResource:
     def on_get(self, req, resp):
@@ -12,5 +12,16 @@ class MainResource:
 
         resp.media = starter
 
+class ObjRequestClass():
+    def on_get(self, req, resp):
+        content = {
+            'name': "Muh yasin",
+            'age': 31,
+            'country': "Indonesia"
+        }
+
+        resp.body = json.dumps(content)
+
 api = falcon.API()
 api.add_route('/', MainResource())
+api.add_route('/test', ObjRequestClass())
